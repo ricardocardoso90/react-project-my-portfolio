51004523b19573b3b1916a17e640301ac4c4eced
Ricardo Cardoso <ricardomanolo90@gmail.com>
2026-04-01 14:08:01 -0300

    update commit

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,12 @@
 import './styles.css';
+import imageTest from '../../images/image-test.png';
 
 export default function Card() {
   return (
-    <div className='card'>
-      <span className='card-title'>[Título do Projeto]</span>
+    <a href='#' target='_blank' rel="noopener noreferrer" className='card'>
+      <span className='card-title'>Título do Projeto</span>
+      <img className='card-image' src={imageTest} alt="Descrição da imagem" />
       <p>Descrição do projeto, tecnologias utilizadas e outros detalhes relevantes.</p>
-    </div>
+    </a>
   )
 };
