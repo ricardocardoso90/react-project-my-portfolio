4a5a04714636f7667c5a9095d55b7e3f010766d0
Ricardo Cardoso <ricardomanolo90@gmail.com>
2026-04-01 20:35:45 -0300

    update commit

--- src/utils/cards-infos.js
+++ src/utils/cards-infos.js
@@ -1,39 +1,45 @@
+import GetAPet from '../images/get-a-pet.jpg';
+import GuessWhat from '../images/guess-what.png';
+import DogDev from '../images/dog-dev.png';
+import DevMemory from '../images/dev-memory.png';
+import IgniteTimer from '../images/ignite-timer.png';
+
 import imageTest from '../images/image-test.png';
 
 export const CardsInfos = [
   {
-    link: 'https://github.com/ricardocardoso90/react-native-project-ignite-teams',
-    title: 'Projeto 1',
-    image: imageTest,
-    description: 'Descrição do projeto 1, tecnologias utilizadas e outros detalhes relevantes.',
+    link: 'https://github.com/ricardocardoso90/node-project-get-a-pet',
+    title: 'Get a Pet',
+    image: GetAPet,
+    description: 'O Get A Pet é um projeto de adoção de animais para consolidar o desenvolvimento Full Stack com Node.js, MongoDB e React.',
   },
 
   {
-    link: 'https://github.com/ricardocardoso90/react-native-project-ignite-teams',
-    title: 'Projeto 2',
-    image: imageTest,
-    description: 'Descrição do projeto 2, tecnologias utilizadas e outros detalhes relevantes.',
+    link: 'https://github.com/ricardocardoso90/react-project-guess-what',
+    title: 'Adivinhe',
+    image: GuessWhat,
+    description: 'O “Adivinhe” é um jogo interativo de adivinhação de palavras, onde o jogador precisa descobrir a palavra correta a partir de uma dica exibida na tela.',
   },
 
   {
-    link: 'https://github.com/ricardocardoso90/react-native-project-ignite-teams',
-    title: 'Projeto 3',
-    image: imageTest,
-    description: 'Descrição do projeto 3, tecnologias utilizadas e outros detalhes relevantes.',
+    link: 'https://github.com/ricardocardoso90/javascript-project-dog-dev',
+    title: 'Dog Dev',
+    image: DogDev,
+    description: 'O DogDev é uma landing page que simula uma empresa fictícia de alimentos para cachorros. A interface traz diversas seções interessantes: vitrine de produtos, depoimentos de empresas parceiras, FAQ, entre outros elementos...',
   },
 
   {
-    link: 'https://github.com/ricardocardoso90/react-native-project-ignite-teams',
-    title: 'Projeto 4',
-    image: imageTest,
-    description: 'Descrição do projeto 4, tecnologias utilizadas e outros detalhes relevantes.',
+    link: 'https://github.com/ricardocardoso90/react-project-dev-memory',
+    title: 'Dev Memory',
+    image: DevMemory,
+    description: 'O DevMemory é um jogo da memória dinâmico e interativo, onde o usuário deve encontrar os pares de cartas correspondentes. A aplicação foi construída com React e TypeScript, unindo performance e tipagem estática para maior robustez.',
   },
 
   {
-    link: 'https://github.com/ricardocardoso90/react-native-project-ignite-teams',
-    title: 'Projeto 5',
-    image: imageTest,
-    description: 'Descrição do projeto 5, tecnologias utilizadas e outros detalhes relevantes.',
+    link: 'https://github.com/ricardocardoso90/react-project-ignite-timer',
+    title: 'Ignite Timer',
+    image: IgniteTimer,
+    description: 'O Ignite Timer é uma aplicação web desenvolvida com React e TypeScript, criada para ajudar o usuário a gerenciar suas tarefas utilizando a técnica Pomodoro. Com ele, é possível registrar uma tarefa, definir o tempo de execução e acompanhar o andamento do ciclo por meio de um timer que inicia automaticamente.',
   },
 
   {
