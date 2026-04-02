0f0909b09cf6160903335babf3111296f3e9abc4
Ricardo Cardoso <ricardomanolo90@gmail.com>
2026-04-02 17:08:46 -0300

    update commit

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function Footer() {
   return (
     <section className="footer">
-      <p className="footer-text">© 2023 Ricardo Cardoso. Todos os direitos reservados.</p>
+      <p className="footer-text">© 2026 Ricardo Cardoso. Todos os direitos reservados.</p>
     </section>
   );
 };
