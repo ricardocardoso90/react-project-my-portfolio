854b4747f54bf00a8170d99abd0f714f179ce86d
Ricardo Cardoso <ricardomanolo90@gmail.com>
2026-04-02 23:59:58 -0300

    update commit

--- src/utils/cards-infos.js
+++ src/utils/cards-infos.js
@@ -1,4 +1,6 @@
-import CalcIMC from '../images/calc-imc.png';
+import CalcIMC from '../images/react/calc-imc.png';
+import DevMemory from '../images/react/dev-memory.png';
+import DogSocialNetwork from '../images/react/dog-social-network.png';
 
 export const CardsInfos = [
   //AQUI VOU COLOCAR OS PROJETOS FEITOS EM REACT.
@@ -8,6 +10,22 @@ export const CardsInfos = [
     title: 'Calc IMC',
     image: CalcIMC,
     category: 'React',
-    description: 'O projeto vai além de apenas calcular o IMC, onde se tem uma Interface clara e intuitiva, exibição de categorias com feedback visual (Magreza, Normal, Sobrepeso e Obesidade).',
+    description: 'O projeto vai além de apenas calcular o IMC, onde se tem uma Interface clara e intuitiva, exibição de categorias com feedback visual (Magreza, Normal, Sobrepeso e Obesidade)...',
+  },
+
+  {
+    link: 'https://github.com/ricardocardoso90/react-project-dev-memory',
+    title: 'Dev Memory',
+    image: DevMemory,
+    category: 'React',
+    description: 'O DevMemory é um jogo da memória dinâmico e interativo, onde o usuário deve encontrar os pares de cartas correspondentes. A aplicação foi construída com React e TypeScript...',
+  },
+
+  {
+    link: 'https://github.com/ricardocardoso90/react-project-dog-social-network',
+    title: 'Dog Social Network',
+    image: DogSocialNetwork,
+    category: 'React',
+    description: 'Nesta plataforma, os usuários podem criar suas contas, compartilhar fotos dos seus pets, além de curtir e comentar nas publicações de outros membros da comunidade...',
   },
 ];
